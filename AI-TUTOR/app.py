c770e88837005a0ae9d8863e9fe3c50106d6eea6
Udaykiran0211 <udaykiran1102@gmail.com>
2026-06-13 00:08:58 +0530

    fix: expose app globaly

--- AI-TUTOR/app.py
+++ AI-TUTOR/app.py
@@ -81,8 +81,9 @@ def create_app(config_class=Config):
 
     return app
 
+app = create_app()
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    app = create_app()
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
